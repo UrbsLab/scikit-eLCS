f1a67bdf821b019453f1b690e79a6ad48671562f
robertfrankzhang <robertzhang100@gmail.com>
2020-03-06 09:42:44 -0500

    Rule Compaction Experiment, std debugging

---
+++ RuleCompaction.py
@@ -0,0 +1,94 @@
+from eLCS import *
+import numpy as np
+import pandas as pd
+from DataCleanup import StringEnumerator
+from sklearn.model_selection import train_test_split
+import copy
+
+class RuleCompacter():
+    def __init__(self,LCS,dataSet,classLabel,cv=3):
+
+        converter = StringEnumerator(dataSet, classLabel)
+        headers, classLabel, dataFeatures, dataPhenotypes = converter.getParams()
+
+        formatted = np.insert(dataFeatures, dataFeatures.shape[1], dataPhenotypes, 1)
+        np.random.shuffle(formatted)
+        dataFeatures = np.delete(formatted, -1, axis=1)
+        dataPhenotypes = formatted[:, -1]
+
+        x_train,x_test,y_train,y_test = train_test_split(dataFeatures,dataPhenotypes,test_size=1/cv)
+
+        self.trainingX = x_train
+        self.trainingY = y_train
+        self.testX = x_test
+        self.testY = y_test
+        self.cv = cv
+        LCS.fit(self.trainingX,self.trainingY)
+
+        self.baseTestScore = LCS.score(self.testX,self.testY)
+        print("Original Score: "+str(self.baseTestScore))
+
+        self.ranking = np.zeros(len(LCS.population.popSet))
+        amountRemove = int(len(LCS.population.popSet) / 4)
+        self.originalPopulation = copy.deepcopy(LCS.population.popSet)
+        oLength = len(LCS.population.popSet)
+
+        for i in range(oLength):
+
+            removedIndexes = []
+            self.population = copy.deepcopy(self.originalPopulation)
+            if i + amountRemove <= oLength:
+                for j in range(amountRemove):
+                    self.population.pop(i)
+                    removedIndexes.append(i+j)
+            else:
+                popEndAmount = oLength - i
+                popBeginAmount = amountRemove - popEndAmount
+                for j in range(popEndAmount):
+                    self.population.pop(i)
+                    removedIndexes.append(i+j)
+                for j in range(popBeginAmount):
+                    self.population.pop(0)
+                    removedIndexes.append(j)
+
+            LCS.population.popSet = copy.deepcopy(self.population)
+            newScore = LCS.score(self.testX,self.testY)
+
+            scoreDiff = self.baseTestScore - newScore
+            for j in removedIndexes:
+                self.ranking[j] += scoreDiff
+            print(str(i)+": "+str(scoreDiff))
+        LCS.population.popSet = self.originalPopulation
+        self.ranking /= amountRemove #Normalize rankings
+        self.indexes = np.argsort(self.ranking) #indexes of worst to best rules
+
+        print()
+        self.worst = self.indexes[:int(oLength/2)]
+        self.best = self.indexes[int(oLength*0.5):]
+
+        newPopNoW = []
+        for i in self.indexes:
+            if i not in self.worst:
+                newPopNoW.append(self.originalPopulation[i])
+
+        LCS.population.popSet = newPopNoW
+        newScore = LCS.score(self.testX, self.testY)
+        print("Score after taking away worst: "+str(newScore))
+
+        newPopNoB = []
+        for i in self.indexes:
+            if i not in self.best:
+                newPopNoB.append(self.originalPopulation[i])
+
+        LCS.population.popSet = newPopNoB
+        newScore = LCS.score(self.testX, self.testY)
+        print("Score after taking away best: "+str(newScore))
+
+        print()
+
+        LCS.population.popSet = copy.deepcopy(self.originalPopulation)
+
+        #for i in self.indexes:
+            #print(LCS.population.popSet[i].fitness)
+            #LCS.printClassifier(self.originalPopulation[i])
+
--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from eLCS import *
 import time
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from RuleCompaction import RuleCompacter
 
 '''Separates out features into np_array of shape [number of items, number of features per item] 
 and labels into np_array of shape [number of items]'''
 
-converter = StringEnumerator("Datasets/Real/Multiplexer11.csv", "class")
-headers, classLabel, dataFeatures, dataPhenotypes = converter.getParams()
+clf = eLCS(learningIterations=5000)
+RuleCompacter(clf,"Datasets/Real/Multiplexer11.csv","class")
 
-clf = eLCS(learningIterations=1000,evalWhileFit=True,trackingFrequency=100,randomSeed=100)
-clf.fit(dataFeatures,dataPhenotypes)
-clf.exportIterationTrackingDataToCSV(filename='DataSets/Tests/RandomTests/track1.csv')
-clf.exportFinalRulePopulationToCSV(headerNames=headers,className=classLabel,ALKR=True,filename='DataSets/Tests/RandomTests/pop1.csv')
-clf.exportFinalPopStatsToCSV(headerNames=headers,filename="DataSets/Tests/RandomTests/popStats1.csv")
-
-clf2 = eLCS(learningIterations=1000,evalWhileFit=True,trackingFrequency=100,randomSeed=100)
-clf2.fit(dataFeatures,dataPhenotypes)
-clf2.exportIterationTrackingDataToCSV(filename='DataSets/Tests/RandomTests/track2.csv')
-clf2.exportFinalRulePopulationToCSV(headerNames=headers,className=classLabel,ALKR=True,filename='DataSets/Tests/RandomTests/pop2.csv')
-clf2.exportFinalPopStatsToCSV(headerNames=headers,filename="DataSets/Tests/RandomTests/popStats2.csv")
+# converter = StringEnumerator("Datasets/Real/Multiplexer11.csv", "class")
+# headers, classLabel, dataFeatures, dataPhenotypes = converter.getParams()
+#
+# clf = eLCS(learningIterations=1000,evalWhileFit=True,trackingFrequency=100,randomSeed=100)
+# clf.fit(dataFeatures,dataPhenotypes)
+# clf.exportIterationTrackingDataToCSV(filename='DataSets/Tests/RandomTests/track1.csv')
+# clf.exportFinalRulePopulationToCSV(headerNames=headers,className=classLabel,ALKR=True,filename='DataSets/Tests/RandomTests/pop1.csv')
+# clf.exportFinalPopStatsToCSV(headerNames=headers,filename="DataSets/Tests/RandomTests/popStats1.csv")
+#
+# clf2 = eLCS(learningIterations=1000,evalWhileFit=True,trackingFrequency=100,randomSeed=100)
+# clf2.fit(dataFeatures,dataPhenotypes)
+# clf2.exportIterationTrackingDataToCSV(filename='DataSets/Tests/RandomTests/track2.csv')
+# clf2.exportFinalRulePopulationToCSV(headerNames=headers,className=classLabel,ALKR=True,filename='DataSets/Tests/RandomTests/pop2.csv')
+# clf2.exportFinalPopStatsToCSV(headerNames=headers,filename="DataSets/Tests/RandomTests/popStats2.csv")
