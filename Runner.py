b996a9cdc7c3bdf228aa85672f9e3c6e3e75f4b5
robertfrankzhang <robertzhang100@gmail.com>
2020-03-10 19:50:12 -0400

    Travis Test 3

---
+++ Runner.py
@@ -0,0 +1,14 @@
+import unittest
+
+import Test_DataCleanup
+import Test_eLCS
+
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+
+suite.addTests(loader.loadTestsFromModule(Test_DataCleanup))
+suite.addTests(loader.loadTestsFromModule(Test_eLCS))
+
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
