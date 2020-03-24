6b511624abc6fd14e9cfb7373eb9d88162fa66f6
robertfrankzhang <robertzhang100@gmail.com>
2020-03-23 20:06:02 -0400

    Travis Updates

--- Runner.py
+++ Runner.py
@@ -4,13 +4,14 @@ import Test_DataCleanup
 import Test_eLCS
 
 
-loader = unittest.TestLoader()
-suite = unittest.TestSuite()
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
 
-suite.addTests(loader.loadTestsFromModule(Test_DataCleanup))
-suite.addTests(loader.loadTestsFromModule(Test_eLCS))
+    suite.addTests(loader.loadTestsFromModule(Test_DataCleanup))
+    suite.addTests(loader.loadTestsFromModule(Test_eLCS))
 
-runner = unittest.TextTestRunner(verbosity=3)
-result = runner.run(suite)
+    runner = unittest.TextTestRunner(verbosity=3)
+    result = runner.run(suite)
 
-print(result)
+    print(result)
